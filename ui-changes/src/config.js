a6ab9e3582b5e5fe8f648b9737f2d16e126053c4
Yibei Chen <yibeichan@gmail.com>
2023-12-21 19:30:43 -0500

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema-demo-protocol/main/reproschema_demo_protocol/reproschema_demo_protocol_schema',
-  banner: 'A demo protocol for the ReproSchema paperr',
+  banner: 'A demo protocol for the ReproSchema paper',
   startButton: 'Join',
   assetsPublicPath: '/reproschema-demo-protocol/',
   backendServer: null
